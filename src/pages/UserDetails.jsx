baf09fef128ab652f1f8d383b6fdf7300dfd9621
Ubongg <ubongeffiong5@gmail.com>
2023-11-04 18:44:43 +0100

    done

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,86 +1,227 @@
-// import { useLoaderData, useParams, useNavigate } from "react-router-dom";
-
-// const UserDetails = () => {
-//   const { id } = useParams();
-//   const userInfo = useLoaderData();
-
-//   const user = userInfo.results.find((u) => u.login.uuid === id);
-
-//   console.log(user);
-
-//   return (
-//     <div className="user-details">
-//       {user && (
-//         <div className="user-preview">
-//           <h2>{`${user.name.first} ${user.name.last}`}</h2>
-//           <p>{user.location.country}</p>
-//           <p>{user.email}</p>
-//           <p>{user.phone}</p>
-//           <p>{user.login.uuid}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default UserDetails;
-
-// // data loader
-// // export const UserDetailsLoader = async ({ params }) => {
-// //   const { id } = params;
-
-// //   const response = await fetch("https://randomuser.me/api/?results=10" + id);
-
-// //   if (!response.ok) {
-// //     throw Error("Could not find that user.");
-// //   }
-
-// //   return response.json();
-// // };
-
-// export const UserDetailsLoader = async ({ params }) => {
-//   const { id } = params;
-
-//   const response = await fetch("https://randomuser.me/api/?results=10");
-
-//   if (!response.ok) {
-//     throw Error("Could not fetch user data.");
-//   }
-
-//   const data = await response.json();
-//   const user = data.results.find((u) => u.login.uuid === id);
-
-//   if (!user) {
-//     throw Error("User not found.");
-//   }
-
-//   return user;
-// };
-
+import { Box, Breadcrumbs, Stack, Typography } from "@mui/material";
 import React from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import "../index.css";
 
 const UserDetails = () => {
+  const theme = useTheme();
   const { id } = useParams();
   const users = useSelector((state) => state.users.users);
 
   const user = users.results.find((u) => u.login.uuid === id);
 
+  const originalDate = user && user.dob.date;
+  const date = new Date(originalDate);
+
+  // Format date as YYYY-MM-DD
+  const formattedDate = date.toISOString().split("T")[0];
+
+  const breadcrumbs = [
+    <Link
+      to={`/`}
+      underline="hover"
+      key="1"
+      style={{
+        textDecoration: "none",
+        color: "black",
+      }}
+    >
+      Home
+    </Link>,
+    <Typography key="2" color="#0066b2">
+      Breadcrumb
+    </Typography>,
+  ];
+
   return (
-    <div className="user-details">
+    <Box className="user-details">
+      <Stack spacing={2}>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          {breadcrumbs}
+        </Breadcrumbs>
+      </Stack>
       {user ? (
-        <div className="user-preview">
-          <h2>{`${user.name.first} ${user.name.last}`}</h2>
-          <p>{user.location.country}</p>
-          <p>{user.email}</p>
-          <p>{user.phone}</p>
-          <p>{user.login.uuid}</p>
-        </div>
+        <Box
+          sx={{
+            display: "flex",
+            // flexDirection: "column",
+            gap: "2rem",
+            alignItems: "center",
+            justifyContent: "center",
+            mt: "3rem",
+            [theme.breakpoints.down("sm")]: {
+              flexDirection: "column",
+              // gap: "1rem",
+            },
+          }}
+        >
+          <Box
+            sx={{
+              width: "40%",
+              height: "50%",
+              [theme.breakpoints.down("sm")]: {
+                width: "100%",
+              },
+            }}
+          >
+            <img
+              src={user.picture.large}
+              alt=""
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "50%",
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: "1.2rem",
+              [theme.breakpoints.down("sm")]: {
+                mb: "2rem",
+                textAlign: "center",
+              },
+            }}
+          >
+            <Typography variant="p">
+              <span
+                style={{
+                  color: "#0066b2",
+                  fontWeight: 600,
+                  textTransform: "uppercase",
+                }}
+              >
+                Full Name:{" "}
+              </span>
+              {`${user.name.first} ${user.name.last}`}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                pl: "10px",
+                [theme.breakpoints.down("sm")]: {
+                  pl: 0,
+                },
+              }}
+            >
+              <span
+                style={{
+                  color: "#0066b2",
+                  fontWeight: 600,
+                  textTransform: "uppercase",
+                }}
+              >
+                Country:{" "}
+              </span>
+              {user.location.country}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                pl: "20px",
+                [theme.breakpoints.down("sm")]: {
+                  pl: 0,
+                },
+              }}
+            >
+              <span
+                style={{
+                  color: "#0066b2",
+                  fontWeight: 600,
+                  textTransform: "uppercase",
+                }}
+              >
+                Phone Number:{" "}
+              </span>
+              {user.phone}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                pl: "30px",
+                [theme.breakpoints.down("sm")]: {
+                  pl: 0,
+                },
+              }}
+            >
+              <span
+                style={{
+                  color: "#0066b2",
+                  fontWeight: 600,
+                  textTransform: "uppercase",
+                }}
+              >
+                Email Address:{" "}
+              </span>
+              {user.email}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                pl: "20px",
+                [theme.breakpoints.down("sm")]: {
+                  pl: 0,
+                },
+              }}
+            >
+              <span
+                style={{
+                  color: "#0066b2",
+                  fontWeight: 600,
+                  textTransform: "uppercase",
+                }}
+              >
+                City:{" "}
+              </span>
+              {user.location.city}
+            </Typography>
+            <Typography
+              variant="p"
+              sx={{
+                pl: "10px",
+                [theme.breakpoints.down("sm")]: {
+                  pl: 0,
+                },
+              }}
+            >
+              <span
+                style={{
+                  color: "#0066b2",
+                  fontWeight: 600,
+                  textTransform: "uppercase",
+                }}
+              >
+                Date of Birth:{" "}
+              </span>
+              {formattedDate}
+            </Typography>
+            <p>
+              <span
+                style={{
+                  color: "#0066b2",
+                  fontWeight: 600,
+                  textTransform: "uppercase",
+                }}
+              >
+                Nationality:{" "}
+              </span>
+              {user.nat}
+            </p>
+          </Box>
+        </Box>
       ) : (
         <p>User not found</p>
       )}
-    </div>
+    </Box>
   );
 };
 
